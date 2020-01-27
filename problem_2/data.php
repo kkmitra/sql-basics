fb5592cad8aaf817dc7817eedf9a3e893d3dd3f3
kkmitra <kaushik.mitra@innoraft.com>
2020-01-27 16:32:33 +0530

    Added styles

--- problem_2/data.php
+++ problem_2/data.php
@@ -29,9 +29,9 @@ if (isset($_GET["q"])) {
         case 7:
             $return_value = $q->GetDomainsWithSalaryLs30();
             break;
-            case 8:
-                $return_value = $q->GetIdWhereEmpCodeIsNull();
-                break;
+        case 8:
+            $return_value = $q->GetIdWhereEmpCodeIsNull();
+            break;
         default:
             echo "Hooooaaaahhhhhhh";
     }
@@ -41,43 +41,86 @@ $data = $return_value[0];
 $columns = $return_value[1];
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-<ul>
-    <li><a href="./data.php?q=1">Query 1 - Get all the data...</a></li>
-    <li><a href="./data.php?q=2">Query 2 - Get first name with salary > 50...</a></li>
-    <li><a href="./data.php?q=3">Query 3 - Get last name with graduation percentile less 70%...</a></li>
-    <li><a href="./data.php?q=4">Query 4 - Get code name with graduation percentile less 70%...</a></li>
-    <li><a href="./data.php?q=5">Query 5 - Get full name that are not of domain Java</a></li>
-    <li><a href="./data.php?q=6">Query 7 - Get employee domain with sum of its salary</a></li>
-    <li><a href="./data.php?q=7">Query 8 - Get employee domain with sum of its salary less than 30k</a></li>
-    <li><a href="./data.php?q=8">Query 9 - Get employee id which has not been assigned employee code</a></li>
-</ul>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <h1><a href="index.php">Problem 7</a></h1>
+    </nav>
+    <div class="data-section">
+        <ul class="query-list">
+            <li><a href="./data.php?q=1" class="query-links">Query 1 - Get all the data...</a></li>
+            <li><a href="./data.php?q=2" class="query-links">Query 2 - Get first name with salary > 50...</a></li>
+            <li><a href="./data.php?q=3" class="query-links">Query 3 - Get last name with graduation percentile less 70%...</a></li>
+            <li><a href="./data.php?q=4" class="query-links">Query 4 - Get code name with graduation percentile less 70%...</a></li>
+            <li><a href="./data.php?q=5" class="query-links">Query 5 - Get full name that are not of domain Java</a></li>
+            <li><a href="./data.php?q=6" class="query-links">Query 7 - Get employee domain with sum of its salary</a></li>
+            <li><a href="./data.php?q=7" class="query-links">Query 8 - Get employee domain with sum of its salary less than 30k</a></li>
+            <li><a href="./data.php?q=8" class="query-links">Query 9 - Get employee id which has not been assigned employee code</a></li>
+        </ul>
 
 
 
-<?php if (!empty($data)) : ?>
-    <div class="mt-3">
+        <?php if (!empty($data)) : ?>
+            <div class="mt-3">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <?php foreach ($columns as $value) : ?>
+                                <th scope="col"><?php echo $value ?></th>
+                            <?php endforeach; ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data as $key_i => $row) : ?>
+                            <tr>
+                                <th scope="row"><?php echo ($key_i + 1); ?></th>
+                                <?php foreach ($row as $value) : ?>
+                                    <th scope="col"><?php echo $value ?></th>
+                                <?php endforeach; ?>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+    </div>
+<?php else : ?>
+    <?php // echo $q->error_msg != "" ? $q->error_msg : "Nothing to show" 
+    ?>
+    <?php if ($q->error_msg != "") : ?>
+        <div class="error-msg">
+            <?php echo $q->error_msg ?>
+        </div>
+    <?php else : ?>
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <?php foreach ($columns as $value) : ?>
-                        <th scope="col"><?php echo $value ?></th>
-                    <?php endforeach; ?>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($data as $key_i => $row) : ?>
-                    <tr>
-                        <th scope="row"><?php echo ($key_i + 1); ?></th>
-                        <?php foreach ($row as $value) : ?>
-                            <th scope="col"><?php echo $value ?></th>
-                        <?php endforeach; ?>
-                    </tr>
-                <?php endforeach; ?>
+                <tr>
+                    <th> </th>
+                    <th>Nothing to show</th>
+                </tr>
             </tbody>
         </table>
-    </div>
-<?php else : ?>
-    <?php echo $q->error_msg != "" ? $q->error_msg : "Nothing to show" ?>
+    <?php endif; ?>
 <?php endif; ?>
+
+</div>
+</body>
+
+</html>
--- problem_2/index.php
+++ problem_2/index.php
@@ -8,7 +8,7 @@ if (isset($_POST["submit"])) {
         header("Location: data.php?q=1");
         exit();
     } else {
-        $emp->error_msg;
+        $GLOBALS["error_msg"] = $emp->error_msg;
     }
 }
 
@@ -22,30 +22,46 @@ if (isset($_POST["submit"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Problem 2</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <div>
-            <input type="text" id="first_name" name="first_name" placeholder="Firstname...">
-        </div>
-        <div>
-            <input type="text" id="last_name" name="last_name" placeholder="Lastname...">
-        </div>
-        <div>
-            <input type="text" id="salary" name="salary" placeholder="Salary...">
-        </div>
-        <div>
-            <input type="text" id="percentile" , name="percentile" placeholder="percentile...">
-        </div>
-        <div>
-            <input type="text" id="domain" name="domain" placeholder="domain...">
-        </div>
-        <div>
-            <input type="text" id="code_name" name="code_name" placeholder="code_name">
-        </div>
-        <input type="submit" value="Submit" name="submit">
-    </form>
+
+    <nav>
+        <h1>Problem 2</h1>
+        <a href="data.php?q=1">View All Data</a>
+    </nav>
+    <div class="container display-flex flex-center direction-column">
+        <?php if(isset($error_msg)):?>
+            <div class="error-msg"><?php echo $error_msg?></div>
+        <?php endif; ?>
+        <form action="index.php" method="post">
+            <div>
+                <input type="text" id="code_name" name="code_name" placeholder="code_name">
+            </div>
+            <div class="grid">
+                <div>
+                    <input type="text" id="first_name" name="first_name" placeholder="Firstname...">
+                </div>
+                <div>
+                    <input type="text" id="last_name" name="last_name" placeholder="Lastname...">
+                </div>
+            </div>
+            <div>
+                <input type="text" id="salary" name="salary" placeholder="Salary...">
+            </div>
+            <div class="grid">
+                <div>
+                    <input type="text" id="domain" name="domain" placeholder="domain...">
+                </div>
+                <div>
+                    <input type="text" id="percentile" , name="percentile" placeholder="percentile...">
+                </div>
+            </div>
+            <input type="submit" value="Submit" name="submit">
+        </form>
+    </div>
 </body>
 
 </html>

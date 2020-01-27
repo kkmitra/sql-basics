78afeb60aa40f2f6d2fe6f8084df2c19552b344f
kkmitra <kaushik.mitra@innoraft.com>
2020-01-27 17:40:51 +0530

    Added styles

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
-<ul>
-    <li><a href="./problem_2/">Problem 2</a></li>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>SQL Basics</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <h1>Php Assignments</h1>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="container">
+            <div class="section">
+                <h2>Problems:</h2>
+                <ul>
+                    <li><a href="./problem_2/" class="btn">Problem 2</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>

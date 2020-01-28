036f4cfb9835afa08e5435f987db1d4ff08a131a
kkmitra <kaushik.mitra@innoraft.com>
2020-01-28 10:31:59 +0530

    Added queries

--- problem_1/queries.sql
+++ problem_1/queries.sql
@@ -4,9 +4,17 @@ select team_name, captain_name from team_details inner join captain_details usin
 -- 2: Get match fixture
 select match_number, T2.team_name TeamA, T3.team_name TeamB from ((match_details T1 inner join team_details T2 on T1.team_a=T2.team_id) inner join team_details T3 on T1.team_b=T3.team_id);
 
-
 -- 3: Get winner of each match
 select match_number, team_name Winner from match_details inner join team_details on match_details.match_won_by=team_details.team_id;
 
 -- 4: which team won the maximum matches
 select team_name Team, count(match_number) total from match_details T1 inner join team_details T2 on T1.match_won_by=T2.team_id group by team_id;
+
+-- 5: Match results
+select match_number, T2.team_name TeamA, T3.team_name TeamB, T4.team_name Winner from 
+(
+	((match_details T1 inner join team_details T2 on T1.team_a=T2.team_id) 
+	inner join team_details T3 on T1.team_b=T3.team_id)
+	inner join team_details T4 on T1.toss_won_by=T4.team_id
+);
+
